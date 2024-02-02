0261b7f38fbb839b020806db38286297c8d6c911
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-02-02 17:41:00 +0700

    add sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,62 @@
+"use client";
+
 import Image from "next/image";
 import Navbar from "./navbar";
-import Sidebar from "./sidebar";
+import {
+  Home as HomeIcon,
+  Text,
+  Files,
+  KeyRound,
+  Building2,
+  LogOut,
+} from "lucide-react";
+import { useState, useEffect } from "react";
+import { Sidebar, SidebarItem } from "./sidebar";
 
 export default function Home() {
+  const [active, setActive] = useState<string | null>("Beranda");
+
+  const handleClick = (text: string) => {
+    setActive(text === active ? null : text);
+  };
+
   return (
-    <div className="h-screen">
-      <Navbar />
-      <Sidebar />
+    <div>
+      <div className="mb-[-4.8rem] relative">
+        <Navbar />
+      </div>
+      <Sidebar>
+        <SidebarItem
+          icon={<HomeIcon size={20} />}
+          text="Beranda"
+          active={active === "Beranda"}
+          onClick={() => handleClick("Beranda")}
+        />
+        <SidebarItem
+          icon={<Text size={20} />}
+          text="Pendaftaran"
+          active={active === "Pendaftaran"}
+          onClick={() => handleClick("Pendaftaran")}
+        />
+        <SidebarItem
+          icon={<Files size={20} />}
+          text="Riwayat"
+          active={active === "Riwayat"}
+          onClick={() => handleClick("Riwayat")}
+        />
+        <SidebarItem
+          icon={<KeyRound size={20} />}
+          text="Akun"
+          active={active === "Akun"}
+          onClick={() => handleClick("Akun")}
+        />
+        <SidebarItem
+          icon={<Building2 size={20} />}
+          text="Apotek"
+          active={active === "Apotek"}
+          onClick={() => handleClick("Apotek")}
+        />
+      </Sidebar>
     </div>
   );
 }
--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -1,12 +1,42 @@
-export default function Sidebar() {
+import { useState, useEffect } from "react";
+
+export function Sidebar({ children }: { children: React.ReactNode }) {
+  return (
+    <aside className="h-screen w-64">
+      <nav className="flex flex-col h-full bg-white border-r shadow-sm pt-32">
+        <ul className="flex-1 px-2">{children}</ul>
+        <div className="flex pb-20 justify-center">
+          <p className="text-gray-400 text-xs font-sans">PsychoPharm</p>
+        </div>
+      </nav>
+    </aside>
+  );
+}
+
+export function SidebarItem({
+  icon,
+  text,
+  active,
+  onClick,
+}: {
+  icon: React.ReactNode;
+  text: string;
+  active: boolean;
+  onClick: () => void;
+}) {
   return (
     <>
-      <div className="flex h-screen bg-gray-200 w-52 border-r-4 px-5">
-        Beranda
-      </div>
-      {/* <div className="container flex mt-20 ml-16 h-[39rem] w-52 border-r-4 px-5">
-        Beranda
-      </div> */}
+      <li
+        className={`flex relative items-center py-2 px-5 my-3 font-medium rounded-md cursor-pointer transition-colors ${
+          active
+            ? "bg-gradient-to-tr from-yellow-300 to-yellow-200"
+            : "hover:bg-yellow-100 text-gray-700"
+        }`}
+        onClick={onClick}
+      >
+        {icon}
+        <span className="w-52 ml-3">{text}</span>
+      </li>
     </>
   );
 }
