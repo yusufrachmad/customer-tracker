56cb511252534b066d9881d8745ac935dde5f5dd
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-02-13 17:59:51 +0700

    modify to SSR

--- app/components/client_layout.tsx
+++ app/components/client_layout.tsx
@@ -1,4 +1,4 @@
-"use client";
+import { authServerSession } from "@/app/lib/auth";
 import Header from "./header";
 import { Sidebar, SidebarItem } from "./sidebar";
 import {
@@ -8,60 +8,38 @@ import {
   KeyRound,
   Building2,
 } from "lucide-react";
-import { useRouter, usePathname } from "next/navigation";
 
-export default function ClientLayout({
+interface User {
+  role: string;
+}
+
+export default async function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const handleClick = (text: string) => {
-    router.push(text === "Beranda" ? "/" : `/${text.toLowerCase()}`);
-  };
+  const session = (await authServerSession()) as User;
   return (
-    <div>
+    <>
       <div className="mb-[-4.8rem] relative">
         <Header />
       </div>
 
       <div className="flex bg-[#fafafa]">
         <Sidebar>
-          <SidebarItem
-            icon={<HomeIcon size={20} />}
-            text="Beranda"
-            active={pathname === "/"}
-            onClick={() => handleClick("Beranda")}
-          />
-          <SidebarItem
-            icon={<Text size={20} />}
-            text="Pendaftaran"
-            active={pathname === "/pendaftaran"}
-            onClick={() => handleClick("Pendaftaran")}
-          />
-          <SidebarItem
-            icon={<Files size={20} />}
-            text="Riwayat"
-            active={pathname === "/riwayat"}
-            onClick={() => handleClick("Riwayat")}
-          />
-          <SidebarItem
-            icon={<KeyRound size={20} />}
-            text="Akun"
-            active={pathname === "/akun"}
-            onClick={() => handleClick("Akun")}
-          />
-          <SidebarItem
-            icon={<Building2 size={20} />}
-            text="Apotek"
-            active={pathname === "/apotek"}
-            onClick={() => handleClick("Apotek")}
-          />
+          <SidebarItem icon={<HomeIcon size={20} />} text="Beranda" />
+          <SidebarItem icon={<Text size={20} />} text="Pendaftaran" />
+          <SidebarItem icon={<Files size={20} />} text="Riwayat" />
+          <SidebarItem icon={<KeyRound size={20} />} text="Akun" />
+          {session?.role === "dinkes" && (
+            <SidebarItem
+              icon={<Building2 size={20} />}
+              text="Pelaporan Apotek"
+            />
+          )}
         </Sidebar>
         <div className="pt-[4.8rem] w-4/5">{children}</div>
       </div>
-    </div>
+    </>
   );
 }
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useRouter, usePathname } from "next/navigation";
+
 export function Sidebar({ children }: { children: React.ReactNode }) {
   return (
     <aside className="h-screen w-1/5">
@@ -14,21 +17,24 @@ export function Sidebar({ children }: { children: React.ReactNode }) {
 export function SidebarItem({
   icon,
   text,
-  active,
-  onClick,
 }: {
   icon: React.ReactNode;
   text: string;
-  active: boolean;
-  onClick: () => void;
 }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const handleClick = (text: string) => {
+    router.push(text === "Beranda" ? "/" : `/${text.toLowerCase()}`);
+  };
+  const active = pathname === `/${text.toLowerCase()}`;
+
   return (
     <>
       <li
         className={`flex relative items-center py-2 px-5 my-3 font-medium rounded-md cursor-pointer transition-colors ${
           active ? "bg-[#f0de36]" : "hover:bg-yellow-100 text-gray-700"
         }`}
-        onClick={onClick}
+        onClick={() => handleClick(text)}
       >
         {icon}
         <span className="w-52 ml-3">{text}</span>
