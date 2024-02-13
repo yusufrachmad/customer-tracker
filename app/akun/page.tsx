b854f4413ae83733e2b292019d1af88248d71e9f
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-02-13 17:58:44 +0700

    update profil page

--- app/akun/page.tsx
+++ app/akun/page.tsx
@@ -1,32 +1,71 @@
-import { getServerSession } from "next-auth";
 import ClientLayout from "../components/client_layout";
-import PageHeader from "../components/page_header";
-import { authOptionsWrapper } from "@/app/api/auth/[...nextauth]/route";
-import {
-  signOut,
-  useSession,
-  SessionProvider,
-  getSession,
-} from "next-auth/react";
-import { cookies, headers } from "next/headers";
+import { authServerSession } from "@/app/lib/auth";
 
 export default async function Akun() {
-  const session = await getServerSession(
-    authOptionsWrapper({ headers: headers(), cookies: cookies() } as any, {
-      params: { nextauth: ["session"] },
-    })[2]
-  );
-  if (!session) {
-    console.log("Session not found");
-  }
+  const session = await authServerSession();
+
   return (
     <ClientLayout>
-      <PageHeader title="Akun" />
-      <div className="px-6 pt-6 pb-3">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Logout
+      <div className="flex border-b px-20 h-20 justify-between items-center bg-[#ffffff]">
+        <h2 className="text-xl font-bold">Profil Apoteker</h2>
+        <button className="flex bg-[#f0de36] rounded-lg w-32 h-10 px-4 ml-10 hover:bg-yellow-300 justify-center items-center shadow-lg">
+          <p className="text-black text-md">Edit</p>
         </button>
       </div>
+      <div className="grid grid-cols-2 gap-y-7 gap-5 mx-20 mt-10">
+        <div className="col-span-1">
+          <h1 className="text-l">Nama Lengkap</h1>
+          <input
+            type="text"
+            className="w-96 h-12 border-2 border-gray-300 rounded-md p-2"
+            placeholder="Nama Lengkap"
+          />
+        </div>
+        <div className="row-span-5">
+          <h1 className="text-l">Alamat Apotek</h1>
+          <textarea
+            className="w-full h-[15rem] border-2 border-gray-300 rounded-md p-2"
+            placeholder="Alamat Apotek"
+          />
+        </div>
+        <div className="col-span-1">
+          <h1 className="text-l">No. STRA</h1>
+          <input
+            type="text"
+            className="w-96  h-12 border-2 border-gray-300 rounded-md p-2"
+            placeholder="No. STRA"
+          />
+        </div>
+        <div className="col-span-1">
+          <h1 className="text-l">No. SIPA</h1>
+          <input
+            type="text"
+            placeholder="No. SIPA"
+            className="w-96 h-12 border-2 border-gray-300 rounded-md p-2"
+          />
+        </div>
+        <div className="col-span-1">
+          <h1 className="text-l">Email</h1>
+          <input
+            type="text"
+            placeholder="Email"
+            className="w-96 h-12 border-2 border-gray-300 rounded-md p-2"
+          />
+        </div>
+        <div className="col-span-1">
+          <h1 className="text-l">Apotek</h1>
+          <input
+            type="text"
+            placeholder="Nama Apotek"
+            className="w-96 h-12 border-2 border-gray-300 rounded-md p-2"
+          />
+        </div>
+      </div>
+      {/* <div className="flex ml-auto mr-20 mt-20 justify-end">
+        <button className="bg-yellow-400 text-black rounded-md p-3 px-10 border-1 border-gray-300 shadow-xl hover:bg-yellow-300">
+          Simpan
+        </button>
+      </div> */}
     </ClientLayout>
   );
 }
