231e23797e30fa5b27ca321fd2e3d4cf07bd36cd
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-02-20 22:40:34 +0700

    add api to find kunjungan

---
+++ app/api/pasien/[id]/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (
+  Request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const req = await Request.json();
+  const { search } = req;
+
+  try {
+    const res = await prisma?.kunjungan.findMany({
+      where: {
+        id_pasien: params.id,
+        OR: [
+          {
+            nama_dokter: {
+              contains: search as string,
+            },
+          },
+        ],
+      },
+    });
+
+    return NextResponse.json({
+      data: res,
+      success: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      error: error?.message,
+    });
+  }
+};
