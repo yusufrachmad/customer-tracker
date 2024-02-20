df6fc4a64fe823d91e067e468136a3a47cca545f
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-02-20 22:43:57 +0700

    add new component in beranda

--- app/components/beranda/kunjungan.tsx
+++ app/components/beranda/kunjungan.tsx
@@ -1,8 +1,15 @@
-export default function Kunjungan() {
+import { getBulanIni } from "../pelaporanapotek";
+
+export default function Kunjungan({
+  jumlahKunjungan,
+}: {
+  jumlahKunjungan: number;
+}) {
+  const bulanIni = getBulanIni();
   return (
     <div className="flex justify-center items-center mt-24">
       <div className="flex flex-col border-2 p-4 rounded-md shadow-sm">
-        Jumlah Kunjungan Bulan XXX: XX
+        Jumlah Kunjungan Bulan {bulanIni}: {jumlahKunjungan}
       </div>
     </div>
   );
