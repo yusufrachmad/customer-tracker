37147057b1bf139307451adb51cccd5ac3da630a
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-02-20 22:40:21 +0700

    add session conditional in searching pasien

--- app/api/pasien/route.ts
+++ app/api/pasien/route.ts
@@ -1,40 +1,47 @@
 import { NextRequest, NextResponse } from "next/server";
+import { authServerSession } from "@/app/lib/auth";
+import type { User } from "@/app/page";
+import prisma from "@/app/lib/db";
 
 export const POST = async (Request: NextRequest) => {
+  const session = (await authServerSession()) as User;
+
   try {
     const req = await Request.json();
     const { search, radio } = req;
+    let apotekId = await prisma?.apoteker.findFirst({
+      where: {
+        id_user: session.id,
+      },
+      select: {
+        id_apotek: true,
+      },
+    });
 
     let fetchResult;
 
-    if (radio === "nama") {
+    if (session?.role === "dinkes") {
       fetchResult = await prisma?.pasien.findMany({
         where: {
-          nama_pasien: {
+          [radio]: {
             contains: search as string,
           },
         },
       });
-    } else if (radio === "nik") {
+    } else if (session?.role === "apoteker") {
       fetchResult = await prisma?.pasien.findMany({
-        where: {
-          nik: {
-            contains: search as string,
+        include: {
+          Apotek: {
+            include: {
+              Apoteker: true,
+            },
           },
         },
-      });
-    } else if (radio === "alamat") {
-      fetchResult = await prisma?.pasien.findMany({
         where: {
-          alamat_ktp: {
-            contains: search as string,
+          Apotek: {
+            id: apotekId?.id_apotek as string,
           },
-        },
-      });
-    } else if (radio === "nama_ibu") {
-      fetchResult = await prisma?.pasien.findMany({
-        where: {
-          nama_ibu: {
+          [radio]: {
             contains: search as string,
           },
         },
@@ -53,11 +60,20 @@ export const POST = async (Request: NextRequest) => {
 };
 
 export const ADD = async (Request: NextRequest) => {
+  const session = (await authServerSession()) as User;
   try {
     const req = await Request.json();
     const { nama_pasien, nik, nama_ibu, tempat_lahir, tanggal_lahir, alamat } =
       req;
     const id = Math.random().toString(36).substring(7);
+    const res = await prisma?.apoteker.findFirst({
+      where: {
+        id_user: session.id,
+      },
+      select: {
+        id: true,
+      },
+    });
 
     await prisma?.pasien.create({
       data: {
@@ -68,6 +84,8 @@ export const ADD = async (Request: NextRequest) => {
         tempat_lahir: tempat_lahir as string,
         tanggal_lahir: new Date(tanggal_lahir as string),
         alamat_ktp: alamat as string,
+        id_apotek: res?.id as string,
+        status: "aktif",
       },
     });
 
