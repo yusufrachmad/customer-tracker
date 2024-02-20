82e7e9b02f4e0fa322db6f74d76e3a1535645c42
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-02-20 22:49:02 +0700

    pendaftaran logic based on session user

--- app/pendaftaran/daftar/page.tsx
+++ app/pendaftaran/daftar/page.tsx
@@ -1,13 +1,32 @@
 import ClientLayout from "@/app/components/client_layout";
 import PageHeader from "@/app/components/page_header";
 import DaftarClient from "@/app/components/pendaftaran/daftar";
+import prisma from "@/app/lib/db";
+import type { Apotek } from "@prisma/client";
+import { authServerSession } from "@/app/lib/auth";
+import type { User } from "@prisma/client";
 
-export default function Daftar() {
+const getApotek = async () => {
+  const res = await prisma?.apotek.findMany({
+    select: {
+      id: true,
+      nama_apotek: true,
+    },
+  });
+
+  return res;
+};
+
+export default async function Daftar() {
+  const session = (await authServerSession()) as User;
+  const user = session?.role;
+  const apotek = (await getApotek()) as Apotek[];
+
   return (
     <div>
       <ClientLayout>
         <PageHeader title="Pendaftaran Pasien" />
-        <DaftarClient />
+        <DaftarClient apotek={apotek} user={user} />
       </ClientLayout>
     </div>
   );
