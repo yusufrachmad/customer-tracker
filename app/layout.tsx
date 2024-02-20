8957f264dc334b5f8ad169433d7471b3014eb516
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-02-20 22:34:54 +0700

    add cdn gchart

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="text/javascript"
+          src="https://www.gstatic.com/charts/loader.js"
+        ></script>
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
