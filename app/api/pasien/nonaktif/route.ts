15ca242b89f8c2186bbbf623b9565d3eb8a5d2ab
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-02-20 22:40:52 +0700

    add api to find pasien who non-active

---
+++ app/api/pasien/nonaktif/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (Request: NextRequest) => {
+  const req = await Request.json();
+  const { search } = req;
+
+  try {
+    const res = await prisma?.pasien.findMany({
+      where: {
+        status: "nonaktif",
+        nama_pasien: {
+          contains: search as string,
+        },
+      },
+      select: {
+        nama_pasien: true,
+        nik: true,
+        tgl_nonaktif: true,
+        Apotek: {
+          select: {
+            nama_apotek: true,
+          },
+        },
+      },
+      orderBy: {
+        tgl_nonaktif: "desc",
+      },
+    });
+
+    return NextResponse.json({
+      data: res,
+      success: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      error: error?.message,
+    });
+  }
+};
